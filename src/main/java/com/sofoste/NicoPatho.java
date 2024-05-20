f0973a8c8c14e514859924ed46e5dd89fb0222f9
Stephane Sob Fouodji <stefan-kruger7@hotmail.com>
2024-05-20 17:15:02 +0200

    Update configurations

--- src/main/java/com/sofoste/NicoPatho.java
+++ src/main/java/com/sofoste/NicoPatho.java
@@ -5,12 +5,13 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
-public class ExampleResource {
+@Path("/start")
+public class NicoPatho {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from Quarkus REST";
+    public String welcome() {
+        return "Welcome to Niko-Patho REST";
     }
 }
+
